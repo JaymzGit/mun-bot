bea952189048ef11c700b08491167e861f02a50c
Jay5teak <58385512+Jay5teak@users.noreply.github.com>
2020-05-17 19:03:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 //Import all libraries or dependecies
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
