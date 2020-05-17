438b7990fdc8022506175a023399d11f6d900115
Jay5teak <58385512+Jay5teak@users.noreply.github.com>
2020-05-17 23:25:49 +0800

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -6,8 +6,8 @@ let role = message.author.role;
 let user = message.author;
 
 //Variables needed for voting commands to work
+const delegates = 0;
 var voted= [];
-const delegates = 2;
 var yesCount = 0;
 var noCount = 0;
 var abstainCount = 0;
@@ -24,9 +24,12 @@ while (votednum < delegates){
 */
 	if(user != voted) {
 		//If delegate performed the command "-vote yes", it will move delegate's discord username into the "voted" list/array.
+		if(!args[0]) {
+			message.channel.send("Please include your option. E.g : -vote yes")
+		}
  		if(args[0].toLowerCase() == "yes"){
- 			message.delete();
  			voted.push(message.member.displayName);
+ 			message.delete();
 			//Doing so will add 1 to the "Yes" count and total vote count which will be revealed at the end of voting.
  			yesCount++;
  			votednum++;
@@ -36,8 +39,8 @@ while (votednum < delegates){
 		}
 		//If delegate performed the command "-vote no", it will move delegate's discord username into the "voted" list/array.
  		if(args[0].toLowerCase() == "no"){
- 			message.delete();
  			voted.push(message.member.displayName);
+ 			message.delete();
  			//Doing so will add 1 to the "No" count and total vote count which will be revealed at the end of voting.
  			noCount++;
  			votednum++;
@@ -47,8 +50,8 @@ while (votednum < delegates){
 		}
 		//If delegate performed the command "-vote abstain", it will move delegate's discord username into the "voted" list/array.
  		if(args[0].toLowerCase() == "abstain"){
- 		message.delete();
  		voted.push(message.member.displayName);
+ 		message.delete();
  		//Doing so will add 1 to the "Abstain" count and total vote count which will be revealed at the end of voting.
  		abstainCount++;
  		votednum++;
@@ -56,20 +59,20 @@ while (votednum < delegates){
   		message.channel.send(`:ballot_box: ${user} has **abstained** from voting.`);
  		return;
 		}
- 		console.log(`Voted: ` + voted);
-	}if (voted.includes(message.member.displayName)) {
+	}else {
 /* If the delegate has already voted, despite of which argument he inputs after "-vote`, it will NOT be counted.
 */
  		if(args[0].toLowerCase() == "yes" || args[0].toLowerCase() == "no" || args[0].toLowerCase() == "abstain"){
+ 		message.delete();
 		message.channel.send(`${user} You already voted!`);
-		message.delete();
+		message.delete(30);
  		return;
 		}
 	}
 	}
     if (votednum == delegates) {
-        message.channel.send(yesCount + ` delegate(s) have voted **Yes**.` + `\n` + noCount + ` delegate(s) have voted **No**.` + `\n` + abstainCount + ` delegate(s) have **abstained** from voting.`)
-    }
+        message.channel.send(yesCount + ` delegate(s) voted **Yes**.` + `\n` + noCount + ` delegate(s) voted **No**.` + `\n` + abstainCount + ` delegate(s) **abstained** from voting.`)
+    } 
 }
 /* To ensure everyone has voted, this whole code block will loop until everyone has voted. */
 // }
