e93133909d4bc5ad37d15a2d1e004582dfd8ae0a
Jay5teak <58385512+Jay5teak@users.noreply.github.com>
2020-05-17 19:04:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
-const Sentry = require('@sentry/node');
 bot.commands = new Discord.Collection();
 
 
